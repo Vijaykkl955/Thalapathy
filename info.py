5b10aa24a8bf203f09862f05bec1d116a1341a98
Vijaykkl955 <thalaivamoviez@gmail.com>
2024-10-31 21:04:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,9 +71,9 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone 
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Mini_Request')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Tamil_Mini')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/tmv_link')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Thalaiva_Moviez')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Thalaiva_Moviez')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/Thalaiva_Moviez')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Mini_Request') # Support Chat Link Without https:// or @
 
 # True Or False
@@ -81,7 +81,7 @@ AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
 PM_SEARCH = bool(environ.get('PM_SEARCH', True))
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
-IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
+IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "False")), False)
 AUTO_FFILTER = is_enabled((environ.get('AUTO_FFILTER', "True")), True)
@@ -106,7 +106,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/How_To_Open_Linkl')
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True))
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'modijiurl.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', '384a1b6a599b42aec68078df1a53c8143b01d74d')
 
